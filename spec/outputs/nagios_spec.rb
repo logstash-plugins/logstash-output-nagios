64119291f8127afe8ac1a18da62a53d16863b54c
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-25 09:23:24 +0200

    Add basic test to the project, including some small refactoring on visibility plus small methods to simplify ideas. Also got the path validation removed as is broken in the LS config side.
    Fixes #2

--- spec/outputs/nagios_spec.rb
+++ spec/outputs/nagios_spec.rb
@@ -1,1 +1,27 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+require_relative "../spec_helper"
+
+describe LogStash::Outputs::Nagios do
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("output", "nagios").new
+    expect { plugin.register }.to_not raise_error
+  end
+
+  describe "send" do
+
+    let(:properties) { { "message" => "This is a message!", "nagios_service" => "nagios_service", "nagios_host" => "nagios_host" } }
+    let(:event)      { LogStash::Event.new(properties) }
+
+    before(:each) do
+      subject.register
+      expect(subject).to receive(:commandfile?).and_return(true)
+    end
+
+    it "send the event to nagios" do
+      expect(subject).to receive(:send_to_nagios)
+      subject.receive(event)
+    end
+  end
+
+end
